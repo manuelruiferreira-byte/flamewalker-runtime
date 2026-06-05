5c78b26d68094d975f236135185b581d5c47d18b
manuelruiferreira-byte <manuelruiferreira@gmail.com>
2026-06-05 20:46:56 +0200

    Add ACE Prism service worker

---
+++ Jester-Intelligence/ACE-Prism-App/sw.js
@@ -0,0 +1,55 @@
+const CACHE_NAME='ace-prism-v1-1-0';
+const APP_SHELL=[
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  '../ACED-Lifestyle/ace-prism.html',
+  '../ACED-Lifestyle/shared/assets/logos/ace-prism.png'
+];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache=>cache.addAll(APP_SHELL))
+      .then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  const request=event.request;
+  if(request.method!=='GET')return;
+
+  if(request.mode==='navigate'){
+    event.respondWith(
+      fetch(request)
+        .then(response=>{
+          const copy=response.clone();
+          caches.open(CACHE_NAME).then(cache=>cache.put(request,copy));
+          return response;
+        })
+        .catch(()=>caches.match(request).then(hit=>hit||caches.match('./index.html')))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then(hit=>{
+      const network=fetch(request).then(response=>{
+        if(response&&response.ok){
+          const copy=response.clone();
+          caches.open(CACHE_NAME).then(cache=>cache.put(request,copy));
+        }
+        return response;
+      }).catch(()=>hit);
+      return hit||network;
+    })
+  );
+});
